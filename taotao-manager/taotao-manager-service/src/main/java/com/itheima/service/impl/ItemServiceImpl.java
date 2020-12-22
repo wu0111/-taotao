04f5fa59599562d8a42d29afb43e18aff7129539
wu0111 <whm>
2020-12-22 14:51:27 +0800

    第二天

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/itheima/service/impl/ItemServiceImpl.java
@@ -0,0 +1,19 @@
+package com.itheima.service.impl;
+
+import com.itheima.pojo.Item;
+import com.itheima.pojo.TaoResult;
+import com.itheima.service.ItemService;
+
+/*
+ *  @创建时间:  2020/12/22 14:34
+ *  @描述：    TODO
+ */
+public class ItemServiceImpl implements ItemService {
+
+    private Item
+
+    @Override
+    public TaoResult<Item> findByPage(Integer page, Integer rows) {
+        return null;
+    }
+}
